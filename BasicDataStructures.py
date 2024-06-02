a5b23e3dccb7305cafde60f0ab6039dc31dd61a2
Sergey <104345367+mrfix1033@users.noreply.github.com>
2024-06-02 12:25:24 +0300

    Create BasicDataStructures.py

---
+++ BasicDataStructures.py
@@ -0,0 +1,27 @@
+# Задача «Длина слова».
+a = "произвольную строку"
+print(len(a))
+
+# Задача «Суммы и разности».
+first = 3
+second = 2
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+# Задача «Среднее арифметическое».
+first = 3
+second = 2
+third = 4
+mean = (first + second + third) / 3
+print(mean)
+
+# Задача «Простые строчки».
+first_string = "Вторник"
+second_string = "Понедельник"
+print(f"{second_string}, {first_string}")
+
+# Задача «Сложная формула».
+a, b, c = 1, 2, 3
+f = a * (b + c)
+print((f ** 3) / 2)
